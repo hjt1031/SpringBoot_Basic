7e07cc298bc63b4b8aee2da020632060e37bccd2
hjt1031 <hjt1031@naver.com>
2022-02-09 14:18:50 +0900

    스프링 빈과 의존관계

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,26 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/*
+   - 스프링 빈을 등록 하는 2가지 방법
+    1. 컴포넌트 스캔과 자동 의존관계 설정(아래 Autowired 가 예시) => 스프링 컨테이너에 연결 해주는 작업 => @Service, @Repository, @Autowired
+    2. 자바 코드로 직접 스프링 빈 등록하기
+
+   - 컴포넌트 스캔과 자동 의존관계 설정
+    1. @Conponent 애노테이션이 있으면 스프링 빈으로 자동 등록됨(@Autowired 안에 @Conponent 가 들어가 있다)
+
+ */
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+// 구현체
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
